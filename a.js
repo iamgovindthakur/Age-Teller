8a6ab40ad81e83e92f6a09a596cfe019071c6899
iamgovindthakur <iamgovindthakur@gmail.com>
2019-08-12 20:09:29 +0530

    basic front end application

---
+++ a.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementById("bday").value;
+  // document.getElementById("demo").innerHTML = x;
+  if(x)
+  window.alert("I got you! Your date of Birth is: "+x);
+
+  else
+  window.alert("oho! please first enter the date of birth ");
+}
